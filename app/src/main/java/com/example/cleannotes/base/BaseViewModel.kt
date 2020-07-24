ceabf8660b9dd8e07352767a22a336d99415b955
slothdeboss <valentynvolodarskij@gmail.com>
2020-07-24 10:06:15 +0300

    BaseViewModel changes.

--- app/src/main/java/com/example/cleannotes/base/BaseViewModel.kt
+++ app/src/main/java/com/example/cleannotes/base/BaseViewModel.kt
@@ -3,8 +3,12 @@ package com.example.cleannotes.base
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.cleannotes.event.Event
 import com.example.domain.state.State
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 abstract class BaseViewModel: ViewModel() {
 
